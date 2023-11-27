d53af2b69aa9d15094a0c4cd7c25762976c04fbb
Austin Limanek <austin.limanek@gmail.com>
2023-11-27 15:51:45 -0800

    created a sign-up flow that showed that the OTP sign up method works for cross browser and cross device sign ups.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -16,16 +16,22 @@ export default function Login({
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
+    console.log(email, password)
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`
+      }
     })
 
     if (error) {
       return redirect('/login?message=Could not authenticate user')
     }
 
-    return redirect('/')
+    console.log(data)
+
+    return redirect(data.url);
   }
 
   const signUp = async (formData: FormData) => {
@@ -51,6 +57,30 @@ export default function Login({
 
     return redirect('/login?message=Check email to continue sign in process')
   }
+
+  const signInWithOtp = async (formData: FormData) => {
+    'use server'
+
+    const email = formData.get('email') as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        // emailRedirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`
+        emailRedirectTo: `https://9855-2601-602-981-dd0-a927-464f-5882-5fab.ngrok-free.app/auth/callback`
+      }
+    })
+
+    if (error) {
+      return redirect('/login?message=Could not authenticate user')
+    }
+
+    return redirect('/login?message=Check email to continue sign in process')
+  }
+
+
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
@@ -106,6 +136,12 @@ export default function Login({
           className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
         >
           Sign Up
+        </button>
+        <button
+          formAction={signInWithOtp}
+          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+        >
+          Sign In/Up with OTP
         </button>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
